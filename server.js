0413c60a463066e9a9402896dc5b2d1be467915b
Adelaaide Grey <dendygrey@gmail.com>
2023-02-26 18:37:58 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const cors = require('cors');
+const knex = require('knex');
+const bcrypt = require('bcrypt-nodejs');
+const register  = require('./controllers/register');
+const signin  = require('./controllers/signin');
+const profile  = require('./controllers/profile');
+const rank = require('./controllers/rank');
+const PORT = process.env.PORT || 3008;
+const { db } = require('pg');
+
+
+const db = knex ({
+      connectionString: process.env.RAILWAY_DATABASE_URL,
+      ssl: {
+        rejectUnauthorised: false
+      }
+  });
+
+  db.connect();
+
+  db.query('SELECT * FROM users', (err, res) => {
+    if (err) throw err;
+    console.log(res.rows);
+    db.end();
+  })
+  
+
+  const app = express();
+  const whitelist = ["http://localhost:3000"]
+  const corsOptions = {
+      origin: function(origin, callback) {
+          if(!origin || whitelist.indexOf(origin) !== -1) {
+              callback(null, true)
+          } else {
+              callback(new Error("Not Allowed by CORS"))
+          }
+      },
+      credentials: true,
+  }
+  
+  app.use(express.json());
+  app.use(cors(corsOptions));
+
+app.get('/', (req, res) => { res.send('it is working') })
+app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)})
+    //2nd thing: Register new users by adding their details to our database.
+app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)})
+    //3rd thing: create user specific IDs
+app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)})
+    //Finally, to get each user's ranks for posting an image surmised, we do:
+app.put('/rank', (req, res) => {rank.handleRank(req, res, db)})
+app.post('/rankurl', (req, res) => {rank.handleApiCall(req, res)})
+
+
+// your code
+
+app.listen(PORT || 3008, () => {
+  console.log(`server started on port ${PORT}`);
+});
+
+
+/**
+ *  --> res = this is working 
+ this is the root route that responds with this is working 
+ * signin --> POST = success/fail
+ this is sign in which will be post request of user information, responding with either success/fail
+ * register --> POST = user
+we have a resgister which will add the new user information to our database, this will return the new created user object.
+ * profile/:userID --> GET = user
+this a user detail that is specific to each user.
+ * ranks/image point --> PUT --> user 
+this keeps a record of each user number
+ */
