48d17057608b210cb032ee091b7061aa55898c07
Adelaaide Grey <dendygrey@gmail.com>
2023-03-02 08:35:53 +0100

    Update signin.js

--- controllers/signin.js
+++ controllers/signin.js
@@ -13,7 +13,6 @@ const handleSignin = (req, res, db, bcrypt)  => {
           return db.select('*').from('users')
             .where('email', '=', email)
             .then(user => {
-                console.log(user);
               res.json(user[0])
             })
             .catch(err => res.status(400).json('unable to get user'))
