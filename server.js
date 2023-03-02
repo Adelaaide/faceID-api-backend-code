d9f714d8de1b00655bae48c8e744260770b5d301
Adelaaide Grey <dendygrey@gmail.com>
2023-03-02 21:33:48 +0100

    Update server.js

--- server.js
+++ server.js
@@ -13,10 +13,12 @@ require('dotenv').config();
 const db = knex({
     client: 'pg',
     connection: {
-        host:process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password:process.env.DB_PASSWORD,
-        database:process.env.DB_NAME,
+        connectionString: "postgres://zfupniia:U1xMSJ0AOp0KWjmYBbpiZibcSNarCvU-@trumpet.db.elephantsql.com/zfupniia",
+        ssl: true
+//         host:process.env.DB_HOST,
+//         user: process.env.DB_USER,
+//         password:process.env.DB_PASSWORD,
+//         database:process.env.DB_NAME,
     }
 });
 
